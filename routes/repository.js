315116ea071fc0310e71bb1b4f32d152f6e4a39d
Hamza Sohail <hamza.sohail@carleton.ca>
2020-12-26 15:12:44 -0500

    Improve file extension validaton

--- routes/repository.js
+++ routes/repository.js
@@ -1,25 +1,34 @@
 const express = require('express');
+
 const router = express.Router();
 const formidable = require('formidable');
 const crypto = require('crypto');
 
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-// Load Stock model
+// Load Image model
 const Image = require('../models/Image');
 
-// Load User model
-const User = require('../models/User');
 
 // TO-DO: deal with zip uploads
 // Upload Image
 router.post('/upload', ensureAuthenticated, (req, res) => {
 	let errors = [];
 	const user = req.user;
-	
-    var form = new formidable.IncomingForm();
 
-    form.parse(req);
+	options = {
+		multiples: true,
+		keepExtensions: true,
+		maxFileSize: 15 * 1024 * 1024, // 15mb
+		maxFields: 2,
+		maxFieldsSize: 1 * 1024 * 1024 // 1mb
+	}
+
+    const form = new formidable.IncomingForm(options);
+
+    form.parse(req, (err, fields, files) => {
+
+	});
 
 	// hash the file name
     form.on('fileBegin', (name, file) => {
@@ -28,9 +37,9 @@ router.post('/upload', ensureAuthenticated, (req, res) => {
 			return;
 		}
 
-		const ext = getExt(file.name);
+		const fileType = file.type.split('/').pop();
 
-		if (!validator(ext)) {
+		if (!fileTypeValidator(fileType)) {
 			errors.push({ msg: 'Only allowed file extensions are: jpg, jpeg, png, gif or zip' });
 			return;
 		}
@@ -40,35 +49,29 @@ router.post('/upload', ensureAuthenticated, (req, res) => {
 		newImage.author = user.name;
 		newImage.name =  file.name;
 
-		file.name = crypto.randomBytes(32).toString('hex') + ext;
+		file.name = crypto.randomBytes(32).toString('hex') + '.' + fileType;
 		file.path = './uploads/' + file.name;
 		
 		newImage.storage = file.path;
 		newImage.save();
-    });
 
-    form.on('file', (name, file) => {
-		console.log('Uploaded ' + file.name);
-    });
+	});
 	
-	res.redirect('../dashboard');
-
-    // if (!image) {
-    //   errors.push({ msg: 'Please select an image' });
-    // }
-	
-	// // check image extension
-	
-	// // check image size
-  
-    // if (errors.length > 0) {
-    //   res.render('dashboard', {
-	// 	user: user,
-	// 	errors
-    //   });
-    // } else {
-    //     // add image to local filesystem and update the DB
-    // }
+	if (errors.length == 0) {
+		form.on('file', (name, file) => {
+			console.log(user.name + ' uploaded ' + file.name);
+			// to-do: add this to a log file
+		});
+		res.redirect('../dashboard');
+	} else {	
+		Image.find({user: req.user}, (err, images) => {
+			res.render('dashboard', {
+				user: req.user, 
+				userImages: images,
+				errors: errors
+			});
+		});
+	}
 });
 
 // Delete
@@ -76,36 +79,20 @@ router.post('/delete', ensureAuthenticated, (req, res) => {
     const images = req.images;
     const user = req.user;
     let errors = [];
-    
-    // check if amount exceeds User's current balance
 
     if (!images) {
       errors.push({ msg: 'Please select image(s) to delete' });
     }
 	
-	// check if user owns the images
+	// to-do: check if user owns the images
   
-    if (errors.length > 0) {
-      res.render('dashboard', {
-		user: user,
-        errors
-      });
-    } else {
-        // remove the images from DB, add them to a queue to be deleted from local file system
-    }
+	// to-do: remove the images from DB, add them to a queue to be deleted from filesystem
+
 });
 
-const getExt = filename => {
-	let ext = filename.substr(filename.length - 5);
-	if (ext === ".jpeg") {
-		return ext;
-	} 
-	ext = filename.substr(filename.length - 4);
-	return ext;
-}
 
-const validator = ext => {
-	return ext === ".zip" || ext === ".jpg" || ext === ".png" || ext == "jpeg" || ext === ".gif"
+const fileTypeValidator = fileType => {
+	return fileType == 'zip' || fileType == 'jpg' || fileType == 'png' || fileType == 'jpeg' || fileType == 'gif'
 }
 
 module.exports = router;
