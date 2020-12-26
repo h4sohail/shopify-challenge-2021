cfb4e8a34026b424a5775a8dd19a89a1a462c8e9
Hamza Sohail <hamza.sohail@carleton.ca>
2020-12-26 13:16:04 -0500

    Add dashboard and repository route

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+const Image = require('../models/Image');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  Image.find({user: req.user}, function(err, images) {
+	res.render('dashboard', {
+		user: req.user, 
+		userImages: images
+	});
+  });
+});
+
+// repository
+router.get('/repository', ensureAuthenticated, (req, res) => {
+	Image.find({}, function(err, images) {
+	   res.render('repository', {
+		   user: req.user, 
+		   images: images
+		});
+	});
+});
+
+module.exports = router;
