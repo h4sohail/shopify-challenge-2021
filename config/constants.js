5515e83f0d1ba066ba929303369c96b8140f0800
Hamza Sohail <hamza.sohail@carleton.ca>
2020-12-29 15:00:23 -0500

    Add prod check for URL

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,14 @@
+const IS_PROD = process.env.IS_PROD == 'true';
+const MONGO_URI = process.env.URI;
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'http://localhost';
-const APP_BASE_URL = URL + `:${PORT}`;
-const MONGO_URI = process.env.URI;
+
+if (IS_PROD) {
+	let APP_BASE_URL = URL;
+} else {
+	APP_BASE_URL = URL + `:${PORT}`;
+}
+
 const API_URL = 'api/v1';
 
 module.exports = {
